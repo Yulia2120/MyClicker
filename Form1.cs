0429a1a07d64120d200ff9e1d4bb0dc579d00e2f
Yulia2120 <ket-dz@rambler.ru>
2022-04-12 19:43:15 +0300

    End

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MyClicker
@@ -15,7 +9,7 @@ namespace MyClicker
         Graphics g;
         Point mouse_location = new Point();
 
-        string[] textR = {"100", "200", "300" };
+        string[] textR = { "100", "200", "300" };
         Font font = new Font("Arial", 14);
         SolidBrush exbrush = new SolidBrush(Color.Yellow);
         int click, missed, upgradecount;
@@ -25,13 +19,13 @@ namespace MyClicker
         {
             InitializeComponent();
             g = CreateGraphics();
-       
+
         }
         private void MonetMove()
         {
             Random random = new Random();
             int x, y, z;
-            if(pB1.Top >= ClientSize.Height)
+            if (pB1.Top >= ClientSize.Height)
             {
                 x = random.Next(0, 300);
                 pB1.Location = new Point(x, 0);
@@ -78,7 +72,7 @@ namespace MyClicker
             click++;
             l_Click.Text = $"Clicked: {click}";
             Sound();
-            {
+        
                 g.DrawString(textR[0], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
                 int res = Convert.ToInt32(textR[0]);
                 sum = click * res;
@@ -89,7 +83,6 @@ namespace MyClicker
                     g.DrawString(textR[1], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
                     res = Convert.ToInt32(textR[1]);
                     sum = click * res;
-
                     labBitcoin.Text = $"Money Per Click: {sum} $";
                 }
                 if (upgradecount == 2)
@@ -98,19 +91,19 @@ namespace MyClicker
                     g.DrawString(textR[2], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
                     res = Convert.ToInt32(textR[2]);
                     sum = click * res;
-
                     labBitcoin.Text = $"Money Per Click: {sum} $";
-                }
-            }
-        }
 
+                }
+            
+        }
+    
 
         private void pB2_Click(object sender, EventArgs e)
         {
             click++;
             l_Click.Text = $"Clicked: {click}";
             Sound();
-            {
+           
                 g.DrawString(textR[0], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
                 int res = Convert.ToInt32(textR[0]);
                 sum = click * res;
@@ -121,7 +114,6 @@ namespace MyClicker
                     g.DrawString(textR[1], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
                     res = Convert.ToInt32(textR[1]);
                     sum = click * res;
-
                     labBitcoin.Text = $"Money Per Click: {sum} $";
                 }
                 if (upgradecount == 2)
@@ -130,10 +122,9 @@ namespace MyClicker
                     g.DrawString(textR[2], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
                     res = Convert.ToInt32(textR[2]);
                     sum = click * res;
-
                     labBitcoin.Text = $"Money Per Click: {sum} $";
                 }
-            }
+            
         }
 
         private void pB3_Click(object sender, EventArgs e)
@@ -141,32 +132,30 @@ namespace MyClicker
             click++;
             l_Click.Text = $"Clicked: {click}";
             Sound();
-            {
-                g.DrawString(textR[0], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
-                int res = Convert.ToInt32(textR[0]);
-                sum = click * res;
-                labBitcoin.Text = $"Money Per Click: {sum} $";
-                if (upgradecount == 1)
-                {
-                    pB3.Image = Properties.Resources.kill;
-                    g.DrawString(textR[1], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
-                    res = Convert.ToInt32(textR[1]);
-                    sum = click * res;
 
-                    labBitcoin.Text = $"Money Per Click: {sum} $";
-                }
-                if (upgradecount == 2)
-                {
-                    pB3.Image = Properties.Resources.kill;
-                    g.DrawString(textR[2], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
-                    res = Convert.ToInt32(textR[2]);
+                    g.DrawString(textR[0], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
+                    int res = Convert.ToInt32(textR[0]);
                     sum = click * res;
-
                     labBitcoin.Text = $"Money Per Click: {sum} $";
-                }
-            }
+                    if (upgradecount == 1)
+                    {
+                        pB3.Image = Properties.Resources.kill;
+                        g.DrawString(textR[1], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
+                        res = Convert.ToInt32(textR[1]);
+                        sum = click * res;
+                        labBitcoin.Text = $"Money Per Click: {sum} $";
+                    }
+                    if (upgradecount == 2)
+                    {
+                        pB3.Image = Properties.Resources.kill;
+                        g.DrawString(textR[2], font, exbrush, mouse_location.X - 50, mouse_location.Y - 50);
+                        res = Convert.ToInt32(textR[2]);
+                        sum = click * res;
+                        labBitcoin.Text = $"Money Per Click: {sum} $";
+                    }
+                
+            
         }
-
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             Sound();
@@ -184,32 +173,22 @@ namespace MyClicker
                 labUpgCount.Text = $"Upgrades Count: {upgradecount}";
                 upgradecost = upgradecost * 2;
                 labUpgCost.Text = $"Upgrade Cost:\n {upgradecost}";
-           
+             
             }
             else
             {
                 MessageBox.Show("You don’t have enough money!", "Error");
             }
         }
-
-
         private void GameResult()
         {
-            //if (pB1.Top >= ClientSize.Height || pB2.Top >= ClientSize.Height || pB3.Top >= ClientSize.Height)
-            //{
-            //    missed++;
-            //    l_Missed.Text = "Missed: " + missed;
-            //}
             if(l_Missed.Text == "Missed: 5")
             {
                 labOver.Visible = true;
                 timer1.Stop();
-                // l_Missed.Visible = false;
-                // l_Click.Visible = false;
+            
             }
-
-
-            if (l_Click.Text == "Clicked: 30")
+            if (l_Click.Text == "Clicked: 50")
             {
                 labOver.Text = "Level Up...";
                 labOver.Visible = true;
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -81,26 +81,6 @@ namespace MyClicker.Properties {
         }
         
         /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap klipartz_com {
-            get {
-                object obj = ResourceManager.GetObject("klipartz.com", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap WwaQ {
-            get {
-                object obj = ResourceManager.GetObject("WwaQ", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Поиск локализованного ресурса типа System.IO.UnmanagedMemoryStream, аналогичного System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream zvon_monet {
